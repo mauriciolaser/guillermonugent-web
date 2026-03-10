63b16910823186bcd9c3e1d89a9507aae0633ef3
mauriciolaser <mauriciocastrovaldez@gmail.com>
2026-03-10 19:38:10 +0100

    mvp release

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,14 @@
-import bannerImg from '../../assets/images/home/banner.webp';
+import bannerDesktop from '../../assets/images/home/banner-desktop.webp';
+import bannerMobile from '../../assets/images/home/banner-mobile.webp';
 import styles from './Banner.module.scss';
 
 function Banner() {
   return (
     <section className={styles.banner}>
-      <img src={bannerImg} alt="Guillermo Nugent" />
+      <picture>
+        <source media="(max-width: 768px)" srcSet={bannerMobile} />
+        <img src={bannerDesktop} alt="Guillermo Nugent" />
+      </picture>
     </section>
   );
 }
